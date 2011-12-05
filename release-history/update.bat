d7daa0596af6ac511a66b738f90dab32f432350e
James McParlane <james@massive.com.au>
2011-12-06 00:40:17 +1100

    Update stubs generated with DrupalUtil.exe that can test automatic updates

---
+++ release-history/update.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\ThumbWhere\DrupalUtil\DrupalUtil.exe"  new ThumbWhere ThumbWhere 7.x http://drupalmodules.thumbwhere.com/release-history/
+"C:\Program Files (x86)\ThumbWhere\DrupalUtil\DrupalUtil.exe" add ..\..\ThumbWhere-Drupal7-Module\ThumbWhere ThumbWhere 7.x patch --recommended --supported --default
